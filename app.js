3a106cf61105cc8957bc5fe292f1615f52eb87b1
Cezain Majeed <63138007+cezainmajeed@users.noreply.github.com>
2021-08-07 18:00:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-cezain:test123@cluster0.kwnnq.mongodb.net/blogDB",{useNewUrlParser:true,useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://",{useNewUrlParser:true,useUnifiedTopology: true});
 
 const postSchema=new mongoose.Schema({
   title:{
